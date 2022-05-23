9726f4e0059413653497dab6acb537bf7bc28181
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-05-23 13:17:02 -0400

    add styling

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path("foods/", views.foods_index, name="index"),
+    path("foods/create/", views.FoodCreate.as_view(), name="foods_create"),
     # accounts url patterns
     path('accounts/signup/', views.signup, name='signup'),
 ]
--- app/views.py
+++ app/views.py
@@ -6,11 +6,22 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import UpdateView, CreateView, DeleteView
 from .models import Food
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+class FoodCreate(LoginRequiredMixin, CreateView):
+
+
+    model = Food
+    fields = ["food_name", "category", "expiry", "shareable", "count"]
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
 def foods_index(request):
